1b2f8c0127ee2f23922bdd1f5e760f62dab02fb5
Sandra Gonzalez <lartenforme@gmail.com>
2021-10-18 10:19:55 +0200

    sécurisé access_control et mise en place role_hierarchy

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Book;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -26,7 +27,10 @@ class DashboardController extends AbstractDashboardController
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        yield MenuItem::linktoDashboard('Catalogue', 'fa fa-book');
+
+        if ($this->isGranted('ROLE_EMPLOYE')) {
+        yield MenuItem::linkToCrud('Liste livre', 'fas fa-list', Book::class);
+        }
     }
 }
